345b612f72bebd8297e141c073480d171d57fe32
Saqib Sharif <saqibmalik202305555@gamil.com>
2026-06-13 22:41:53 -0700

    signup frontend done

--- client/src/pages/SigninPage.jsx
+++ client/src/pages/SigninPage.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 const SigninPage = () => {
+  const handletemp   = (e) => {
+    try{
+      fetch('http://localhost:5000/api/temp')
+      .then(res => res.text())
+      .then(data => alert(data))
+    }
+    catch(err){
+      console.error(err);
+    }// Handle sign in logic here
+  }
   return (
     <div className='registerPage'>
         <form>
             <h2>Sign In</h2>
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <button type="submit">Sign In</button>
+            <button onClick={handletemp} type="submit">Sign In</button>
         </form>
 
     </div>
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,13 +1,51 @@
 import React from 'react'
+import {useState} from 'react';
 
 const SignupPage = () => {
+  const [formData, setFormData] = useState({
+    id: Date.now(), // Generate a unique ID based on the current timestamp
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const handleSignup=(e)=>{
+    e.preventDefault();
+    try{
+      fetch('http://localhost:5000/api/users',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(formData)
+      });
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  }
+
   return (
     <div className='registerPage'>
-      <form>
+      <form onSubmit={handleSignup}>
         <h2>Sign Up</h2>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input 
+          type="text" 
+          placeholder="Name" 
+          value={formData.name}
+          onChange={(e) => setFormData({...formData, name: e.target.value})}
+        />
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={formData.email}
+          onChange={(e) => setFormData({...formData, email: e.target.value})}
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={formData.password}
+          onChange={(e) => setFormData({...formData, password: e.target.value})}
+        />
         <button type="submit">Sign Up</button>
       </form>
     </div>
